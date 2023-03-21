53f3a8a0f13756391af3b7fc42135c7e8c5860c3
jianhua <wangjianhua@tslsmart.com>
2023-03-21 11:00:16 +0800

    小程序demo代码 1

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,30 @@
+Component({
+    data: {
+        selected: 0,
+        color: "#7A7E83",
+        selectedColor: "#3cc51f",
+        list: [{
+            pagePath: "/pages/tabBar/component/index",
+            iconPath: "/resources/image/icon_component.png",
+            selectedIconPath: "/resources/image/icon_component_HL.png",
+            text: "组件"
+        }, {
+            pagePath: "/pages/tabBar/API/index",
+            iconPath: "/resources/image//icon_API.png",
+            selectedIconPath: "/resources/image//icon_API_HL.png",
+            text: "接口"
+        }]
+    },
+    attached() {
+    },
+    methods: {
+        switchTab(e) {
+            const data = e.currentTarget.dataset
+            const url = data.path
+            wx.switchTab({ url })
+            this.setData({
+                selected: data.index
+            })
+        }
+    }
+})
