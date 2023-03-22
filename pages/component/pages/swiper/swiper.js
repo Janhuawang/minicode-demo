1d86b0ad3e623d4b26e045545bf88c7f4c1417dc
jianhua <wangjianhua@tslsmart.com>
2023-03-22 17:27:19 +0800

    视图swiper、引入weui.wxss

--- pages/component/pages/swiper/swiper.js
+++ pages/component/pages/swiper/swiper.js
@@ -1,6 +1,31 @@
 Page({
   data: {
+    imgUrls: ['item-image1', 'item-image2', 'item-image3'],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 500,
 
+  },
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
   },
   onLoad(options) {
 
