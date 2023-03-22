0023b3014a085d0586c1e95c23bcd5890a3c30b0
jianhua <wangjianhua@tslsmart.com>
2023-03-22 10:18:47 +0800

    列表view展示

--- pages/tabBar/component/index.js
+++ pages/tabBar/component/index.js
@@ -8,5 +8,27 @@ Component({
         })
       }
     }
+  },
+  data: {
+    list: [
+      {
+        id: "view",
+        name: "视图容器",
+        open: false,
+        pages: ['view', 'scroll-view', 'swiper']
+      },
+      {
+        id: "conent",
+        name: "基础内容",
+        open: false,
+        pages: ['text', 'icon', 'progress']
+      },
+      {
+        id: "form",
+        name: "表单组件",
+        open: false,
+        pages: ['button', 'checkbox', 'form', 'input', 'label', 'picker', 'radio', 'slider', 'switch', 'textarea']
+      },
+    ]
   }
 })
